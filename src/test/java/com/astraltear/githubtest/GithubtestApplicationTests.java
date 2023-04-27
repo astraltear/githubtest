573bcc8a40e491bf26251f02d1ee08d4bee7bacd
astraltear <astraltear@gmail.com>
2023-04-27 09:19:33 +0900

    a

---
+++ src/test/java/com/astraltear/githubtest/GithubtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astraltear.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
