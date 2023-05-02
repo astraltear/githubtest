abe60ce5b107e8330614ba96ed031e3a76861670
astraltear <astraltear@gmail.com>
2023-05-02 10:36:08 +0900

    Update GithubtestApplication.java

--- src/main/java/com/astraltear/githubtest/GithubtestApplication.java
+++ src/main/java/com/astraltear/githubtest/GithubtestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubtestApplication {
 
     public static void main(String[] args) {
-
+        // test annotation
         SpringApplication.run(GithubtestApplication.class, args);
         System.out.println("GithubtestApplication  main");
     }
