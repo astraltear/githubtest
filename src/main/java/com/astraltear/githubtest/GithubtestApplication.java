818c76084ee41099849bba5fae6567bccf0f49cf
astraltear <astraltear@gmail.com>
2023-05-02 10:27:33 +0900

    add main sysout

--- src/main/java/com/astraltear/githubtest/GithubtestApplication.java
+++ src/main/java/com/astraltear/githubtest/GithubtestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubtestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GithubtestApplication.class, args);
+        System.out.println("GithubtestApplication  main");
     }
 
 }
