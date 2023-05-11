b3c06ee59e175e0a0a14d16d3ff3c0ea293ce04c
yjchoi <yjchoi@strato.co.kr>
2023-05-11 10:55:17 +0900

    dev controller

---
+++ src/main/java/com/astraltear/githubtest/controller/DevController.java
@@ -0,0 +1,16 @@
+package com.astraltear.githubtest.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevController {
+
+    @GetMapping("/dev")
+    public HttpEntity<String> home() {
+        return ResponseEntity.ok()
+                .body("devHome");
+    }
+}
