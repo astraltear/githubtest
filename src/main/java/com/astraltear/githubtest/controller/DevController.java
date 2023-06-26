a81a42179c5283cdd22029d61a737f85823ad6ef
yjchoi <yjchoi@strato.co.kr>
2023-06-26 13:29:19 +0900

    clean master

--- src/main/java/com/astraltear/githubtest/controller/DevController.java
+++ src/main/java/com/astraltear/githubtest/controller/DevController.java
@@ -15,27 +15,6 @@ public class DevController {
     }
 
 
-    @GetMapping("/dev/masteradd")
-    public HttpEntity<String> devmasteradd() {
-        return ResponseEntity.ok()
-                .body("devmasteradd");
-    }
 
-    @GetMapping("/dev/devadd")
-    public HttpEntity<String> devadd() {
-        return ResponseEntity.ok()
-                .body("devmasteradd");
-    }
 
-    @GetMapping("/dev/devadd3")
-    public HttpEntity<String> devadd3() {
-        return ResponseEntity.ok()
-                .body("devmasteradd3");
-    }
-
-    @GetMapping("/dev/devadd3_1")
-    public HttpEntity<String> devadd3_1() {
-        return ResponseEntity.ok()
-                .body("devadd3_1");
-    }
 }
